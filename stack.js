11abe55215be9ad5e3d3b9e3367bb2d1d2940971
www5826 <www.wjp5826.com>
2018-03-04 20:58:10 +0800

    perfect stack

--- stack.js
+++ stack.js
@@ -32,3 +32,37 @@ function Stack() {
 		return items.length;
 	}
 }
+// 另一种实现
+let Stack1 = (function(){
+	const items = new WeakMap();
+	class Stack {
+		constructor(){
+			items.set(this, []);
+		}
+		// 入栈，添加一个新元素到栈顶
+		push(element){
+			items.get(this).push(element);
+		}
+		// 出栈方法
+	pop(){
+		return items.get(this).pop();
+	}
+	// peek 返回栈顶的元素
+	peek(){
+		return items.get(this)[items.length - 1];
+	}
+	// 判断栈是否为空,如果为空 返回 true，否则返回 false
+	isEmpty(){
+		return items.get(this).length === 0;
+	}
+	// 移除栈里的所有元素
+	clear() {
+		items.set(this, []);
+	}
+	// 返回栈里元素的个数
+	size(){
+		return items.get(this).length;
+	}
+}
+return Stack;
+})();
